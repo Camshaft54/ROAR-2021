51d8945daf7e3db9d7b04d556c5d3ba191fa71d4
Camshaft54 <39676304+Camshaft54@users.noreply.github.com>
2021-08-27 10:40:20 -0700

    Add setup_pitstop function and note in README.md for submission.

--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
-test
+# Robot Open Autonomous Racing (ROAR)
+
+### To Contribute
+- Please click the [Fork](https://docs.github.com/en/free-pro-team@latest/github/getting-started-with-github/fork-a-repo) button on the upper right corner and submit a pull request to master branch.
+    - For a more in-depth tutorial on recommended setup [video](https://youtu.be/VA13dAZ9iAw)
+- Please follow suggested guidelines on Pull Request. 
+
+### Quick start
+For quick start documentation, please visit our documentation site: [https://augcog.github.io/ROAR/quickstart/](https://augcog.github.io/ROAR/quickstart/)
+
+
+### Enter the Competition
+Visit [https://augcog.github.io/ROAR/competition_instruction/](https://augcog.github.io/ROAR/competition_instruction/)
+
+### How to run my code
+There should not be any new dependencies, so if the conda environment used has already been set up like [this](https://augcog.github.io/ROAR/quickstart/), then that will be sufficient.
+This should be as simple as running `runner_competition_evaluator.py` after starting the Berkeley minor server.
+There seems to be a bug with the competition evaluator where the car will start at the starting position, teleport to [0,0,0], then go back to the start position when the simulator is first started.
+I was able to fix this in `carla_runner.py` (the code is [here](https://gist.github.com/Camshaft54/f02343f82288d4de32f751016299cdef)), but since we are not allowed to modify this for the code for the competition, I have not included it in this submission.
+In order to have the car actually race for 10 laps, I modified num_laps in `runner_competition_evaluator.py` to be 11 (to account for skipping the first lap).
--- runner_competition_evaluator.py
+++ runner_competition_evaluator.py
@@ -9,6 +9,10 @@ from ROAR_Sim.carla_client.carla_runner import CarlaRunner
 from typing import Tuple
 from prettytable import PrettyTable
 from ROAR.agent_module.pid_agent import PIDAgent
+from pit_stop import PitStop as PitStop
+from ROAR_Sim.carla_client.util.utilities import CarlaCarColor, CarlaCarColors
+import os
+import time
 
 
 def compute_score(carla_runner: CarlaRunner) -> Tuple[float, int, int]:
@@ -46,17 +50,20 @@ def run(agent_class, agent_config_file_path: Path, carla_config_file_path: Path,
     agent_config: AgentConfig = AgentConfig.parse_file(agent_config_file_path)
     carla_config = CarlaConfig.parse_file(carla_config_file_path)
 
+    set_up_pitstop(agent_config, carla_config)
+
     # hard code agent config such that it reflect competition requirements
     agent_config.num_laps = num_laps
     carla_runner = CarlaRunner(carla_settings=carla_config,
                                agent_settings=agent_config,
                                npc_agent_class=PurePursuitAgent,
                                competition_mode=True,
+                               start_bbox=np.array([-815, 20, -760, -770, 120, -600]),
                                lap_count=num_laps)
     try:
         my_vehicle = carla_runner.set_carla_world()
         agent = agent_class(vehicle=my_vehicle, agent_settings=agent_config)
-        carla_runner.start_game_loop(agent=agent, use_manual_control=True)
+        carla_runner.start_game_loop(agent=agent, use_manual_control=False)
         return compute_score(carla_runner)
     except Exception as e:
         print(f"something bad happened during initialization: {e}")
@@ -77,9 +84,9 @@ def suppress_warnings():
 def main():
     suppress_warnings()
     agent_class = PIDAgent
-    num_trials = 2
+    num_trials = 10
     total_score = 0
-    num_laps = 2
+    num_laps = 11
     table = PrettyTable()
     table.field_names = ["time_elapsed (sec)", "num_collisions", "laps completed"]
     for i in range(num_trials):
@@ -89,6 +96,75 @@ def main():
                      num_laps=num_laps)
         table.add_row(scores)
     print(table)
+
+
+def set_up_pitstop(agent_config, carla_config):
+    pitstop = PitStop(carla_config, agent_config)
+    pitstop.set_carla_sync_mode(synchronized=False)
+    pitstop.set_autopilot_mode(enabled=True)
+    pitstop.set_car_model(car_model="vehicle.nissan.micra")
+    pitstop.set_car_color(color=CarlaCarColor(r=255, g=200, b=00, a=255))
+    pitstop.set_num_laps(num=1)
+    pitstop.set_waypoint_file_path(path=(Path(
+        os.getcwd()) / "ROAR_Sim" / "data" / "berkeley_minor_waypoints.txt").as_posix())
+    pitstop.set_output_data_folder_path(path="./data/output")
+    pitstop.set_output_data_file_name(name=time.strftime("%Y%m%d-%H%M%S-") + "map-waypoints")
+    pitstop.set_max_speed(speed=200)
+    pitstop.set_target_speed(speed=90)
+    pitstop.set_steering_boundary(boundary=(-1.0, 1.0))
+    pitstop.set_throttle_boundary(boundary=(0, 1.0))
+    pitstop.set_waypoints_look_ahead_values(values={
+        "60": 5,
+        "80": 10,
+        "120": 20,
+        "180": 50})
+    pid_values = {
+        "longitudinal_controller": {
+            "40": {
+                "Kp": 1.0,
+                "Kd": 0.3,
+                "Ki": 0
+            },
+            "60": {
+                "Kp": 0.7,
+                "Kd": 0.2,
+                "Ki": 0
+            },
+            "80": {
+                "Kp": 0.5,
+                "Kd": 0.1,
+                "Ki": 0
+            },
+            "100": {
+                "Kp": 0.5,
+                "Kd": 0.15,
+                "Ki": 0.05
+            },
+            "150": {
+                "Kp": 0.2,
+                "Kd": 0.1,
+                "Ki": 0.1
+            }
+        },
+        "latitudinal_controller": {
+            "60": {
+                "Kp": 0.8,
+                "Kd": 0.1,
+                "Ki": 0.2
+            },
+            "100": {
+                "Kp": 1.0,
+                "Kd": 0.1,
+                "Ki": 0.2
+            },
+            "150": {
+                "Kp": 0.5,
+                "Kd": 0.2,
+                "Ki": 0.2
+            }
+        }
+    }
+    pitstop.set_pid_values(pid_values)
 
 
 if __name__ == "__main__":
